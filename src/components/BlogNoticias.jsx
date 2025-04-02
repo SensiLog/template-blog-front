2c11ba2e644bb056fd1493681265f84004c91ea8
César <cesardsbraz@gmail.com>
2025-04-02 10:38:57 -0300

    ajuste nas rotas

--- src/components/BlogNoticias.jsx
+++ src/components/BlogNoticias.jsx
@@ -31,7 +31,7 @@ function BlogNoticias() {
         <div className='bg-slate-100 flex flex-col items-center px-5 py-10'>
             <div className='flex flex-col items-start w-full 2xl:w-[1500px] lg:px-32 md:px-12'>
                 {data == undefined ? <p>Carregando...</p> : data.map((item, index) => (
-                    <PostBlog key={index} urlImage={item.urlImage} title={item.title} date={item.date} desc={item.desc} />
+                    <PostBlog key={index} id={item.id} urlImage={item.urlImage} title={item.title} date={item.date} desc={item.desc} />
                 ))}
             </div>
         </div>
--- src/components/MaisNoticias.jsx
+++ src/components/MaisNoticias.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function MaisNoticias(props) {
     return (
-        <div className='mt-6'>
-            <h1>{props.titulo}</h1>
-            <div className='flex flex-row justify-between'>
-                <div className='w-[100px] h-[100px] flex-shrink-0 mr-4'>
-                    <img
-                        className="w-full h-full object-cover"
-                        src={props.urlImage}
-                        alt="img-noticia"
-                    />
+        <NavLink to={`/noticia/${props.id}`} className='w-full'>
+            <div className='mt-6'>
+                <h1>{props.titulo}</h1>
+                <div className='flex flex-row justify-between gap-4'>
+                    <div className='w-[100px] h-[100px] flex-shrink-0'>
+                        <img
+                            className="w-full h-full object-cover"
+                            src={props.urlImage}
+                            alt="img-noticia"
+                        />
+                    </div>
+                    <p className='truncate-custom text-slate-600 text-sm'>{props.desc}</p>
                 </div>
-                <p className='truncate-custom text-slate-600 text-sm'>{props.desc}</p>
             </div>
-        </div>
+        </NavLink>
     )
 }
 
