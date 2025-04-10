0c215bb5bffb8fa0d6a66d734374c29b201fc52c
César <cesardsbraz@gmail.com>
2025-04-10 19:32:01 -0300

    fix estilo doclei

--- src/components/DocLei.jsx
+++ src/components/DocLei.jsx
@@ -5,7 +5,7 @@ function DocLei(props) {
     const textoFormatado = formatTextWithLineBreaks(props.conteudo);
 
     return (
-        <div className='bg-white mt-6 rounded-md shadow-md p-8 md:p-12'>
+        <div className='bg-white mt-6 rounded-md shadow-md p-8 md:p-12 max-w-[1200px] w-full'>
             <h1 className='font-bold md:text-lg text-end'>Autor(es): {props.autor}</h1>
             <p className='font-medium text-sm md:text-base' dangerouslySetInnerHTML={{ __html: textoFormatado }} />
         </div>
