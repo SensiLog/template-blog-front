df057665a6abe82cabeacb759774f70ab2fbad47
César <cesardsbraz@gmail.com>
2025-04-11 12:40:25 -0300

    add botão de voltar post noticias e post leis

--- src/admin/PostNoticias.jsx
+++ src/admin/PostNoticias.jsx
@@ -37,7 +37,10 @@ function PostNoticias() {
 
     return (
         <div className='flex flex-col items-center justify-center min-h-screen bg-slate-950 text-white'>
-            <h1 className='text-3xl mb-8 font-bold'>Postar Noticias</h1>
+            <h1 className='text-3xl mb-4 font-bold'>Postar Noticias</h1>
+            <button className='bg-slate-700 cursor-pointer hover:bg-slate-600 transition text-xl font-medium text-white px-4 py-2 rounded mb-4' onClick={() => window.history.back()}>
+                Voltar
+            </button>
             <form
                 className='bg-slate-800 px-6 py-8 rounded shadow-md max-w-[550px] w-80 md:w-1/2 lg:w-1/3'
                 onSubmit={handleSubmit}
